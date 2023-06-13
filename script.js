aaaa615384f7c1d313fead7b1e18f66abf8bd3e7
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-13 16:12:12 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
-
-class Square extends Animal {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width
+		this.height = height
+	}
+	getWidth(){
+		return this.width
+	}
+	getHeight(){
+		return this.height
+	}
+	getArea(){
+		return this.width * this.height
+	}
+}
+	let rec = new Rectangle(4,4)
+console.log(rec.getArea())
+class Square extends Rectangle {
+	getPerimeter(){
+		return 4*rec.width
+	}
+}
+let squa = new Square()
+console.log(squa.getPerimeter())
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
